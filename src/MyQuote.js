99d0937b5446ff74d3c9e8d854cd77b28d6db972
Juan <jcsantamar@gmail.com>
2025-06-13 20:44:00 +0200

    componentes sobre Wikipedia

---
+++ src/MyQuote.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function MyQuote() {
+  return (
+    <blockquote>
+      <p>What is important now is to recover our senses y mas allá.</p>
+      <cite>
+        <a target="_blank" href="https://en.wikipedia.org/wiki/Susan_Sontag">
+          Susan Sontag
+        </a>
+      </cite>
+    </blockquote>
+  );
+}
+
+export default MyQuote;
