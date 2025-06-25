560e2a30f42f508e5325ca40910bd710be762623
Juan <jcsantamar@gmail.com>
2025-06-25 17:05:06 +0200

    Distribucion para aislar diferentes apps. Ultimo commit OK antes de pruebas de recuperacion

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom/client';
 // import MyFooter from './Footer';
 
 // App para manejo basico de Tasks (todos los componentes en subdir aislado). Solo se cambia aqui la llamada al principal
-import Tasks from './appTasks/AppFunction';
+import Tasks from './tasks/AppFunction';
 ReactDOM.createRoot(document.getElementById('app')).render(<Tasks />);
